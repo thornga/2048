adebd52a20d3059768c365f68e67fa61eefc59ed
thornga <americacoingameing@gmail.com>
2025-10-27 09:16:33 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -69,7 +69,7 @@ GameManager.prototype.addStartTiles = function () {
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     //var value = Math.random() < 0.9 ? 2 : 4;
-    var value = 2;
+    var value = 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
