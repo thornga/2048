1963a72fd4d51306dc3b2d448853115ddbcac872
thornga <americacoingameing@gmail.com>
2025-10-27 14:38:38 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -181,6 +181,7 @@ GameManager.prototype.move = function (direction) {
 
   if (moved) {
     this.addRandomTile();
+    this.addRandomTile();
 
     if (!this.movesAvailable()) {
       this.over = true; // Game over!
